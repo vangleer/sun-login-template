863795062f440c1a1f202809e098fbf9a8382be3
huangwantong <965002107@qq.com>
2022-08-13 17:42:41 +0800

    init: vue3

--- App.vue
+++ App.vue
@@ -1,6 +1,17 @@
 <script>
-	export default {			}
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
 </script>
 
-<style>	/* ==================	        初始化	 ==================== */	page {		background-color: #FFFFFF;		font-size: 30rpx;		color: #272e2d;		font-family: Helvetica Neue, Helvetica, sans-serif;	}	view,	scroll-view,	swiper,	button,	input,	textarea,	label,	navigator,	image {		box-sizing: border-box;	}
+<style>
+	/*每个页面公共css */
 </style>
---
+++ pages/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		{{ count }}
+	</view>
+	
+	<button @click="handleAdd">add</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function handleAdd() {
+	count.value++
+}
+</script>
+
+<style>
+
+</style>
