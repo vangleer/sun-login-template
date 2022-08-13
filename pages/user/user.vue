6a08a4aa94f1f85f9477a1962400e7d295cc82da
huangwantong <965002107@qq.com>
2022-08-13 18:11:15 +0800

    chore: 新增tabbar页面

---
+++ pages/user/user.vue
@@ -0,0 +1,14 @@
+<template>
+  
+  <button @click="handleLogout">退出登录</button>
+</template>
+
+<script setup>
+function handleLogout() {
+  uni.redirectTo({
+  	url: '/pages/login/login'
+  })
+}
+</script>
+
+<style lang="scss" scoped></style>
