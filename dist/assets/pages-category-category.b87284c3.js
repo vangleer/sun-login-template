446df21baa87a8848ffc2500b6cb21d745544d2b
huangwantong <huangwantong@escopetech.com>
2022-08-15 11:20:05 +0800

    chore: build

---
+++ dist/assets/pages-category-category.b87284c3.js
@@ -0,0 +1,1 @@
+import{o as a,c as e,w as s,b as r,h as t}from"./index.da5cdabd.js";import{_ as d}from"./plugin-vue_export-helper.21dcd24c.js";var o=d({data:()=>({}),methods:{}},[["render",function(d,o,n,c,p,u){const f=t;return a(),e(f,{class:"sun-page"},{default:s((()=>[r(" Category ")])),_:1})}]]);export{o as default};
